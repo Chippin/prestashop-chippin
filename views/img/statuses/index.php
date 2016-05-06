d74efe66e6dc37a31a2816c8a9fe5f841e162709
mikelovely <mike-lovely@hotmail.co.uk>
2016-05-06 10:08:53 +0100

    change copyright text

--- views/img/statuses/index.php
+++ views/img/statuses/index.php
@@ -17,12 +17,10 @@
 /** Do not edit or add to this file if you wish to upgrade Prestashop to newer
  * versions in the future.
  * ****************************************************
- * @category   Belvg
- * @package    Belvg_BlueSnap
- * @author    Alexander Simonchik <support@belvg.com>
+ * @category   Chippin
+ * @author    Simpleweb
  * @site
- * @copyright  Copyright (c) 2010 - 2015 BelVG LLC. (http://www.belvg.com)
- * @license    http://store.belvg.com/BelVG-LICENSE-COMMUNITY.txt
+ * @copyright  Copyright (c) 2016 Simpleweb.
  */
 
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
